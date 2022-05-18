6cc670a4e17ab0c8d56db9fac86ca85d038af9dc
matschlue <schluetermat@gmail.com>
2022-05-18 17:41:59 -0500

    message

--- schema.sql
+++ schema.sql
@@ -35,4 +35,22 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE dept_emp(
+emp_no INT NOT NULL,
+dept_no VARCHAR(6) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE titles(
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, from_date)
+);
+
 SELECT * FROM departments;
